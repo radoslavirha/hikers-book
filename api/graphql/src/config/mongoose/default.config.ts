b380fcadc060e6ff754242674ed2be0e6dbfdcb6
Radoslav Irha <radoslav.irha@gmail.com>
2023-09-29 12:21:57 +0200

    chore: docker updates

--- api/graphql/src/config/mongoose/default.config.ts
+++ api/graphql/src/config/mongoose/default.config.ts
@@ -1,5 +1,5 @@
 export default {
   id: 'admin',
-  url: process.env.DEFAULT_URL || 'mongodb://root:example@localhost:27017/admin',
+  url: process.env.DEFAULT_URL || 'mongodb://root:password@localhost:27017/admin',
   connectionOptions: {}
 };
