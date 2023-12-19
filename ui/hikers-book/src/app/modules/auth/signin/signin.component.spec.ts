06119738722fe189130e74efb9ad7d3de3e3c7a0
Radoslav Irha <radoslav.irha@gmail.com>
2023-12-19 21:15:06 +0100

    test: fix tests for sign in component

--- ui/hikers-book/src/app/modules/auth/signin/signin.component.spec.ts
+++ ui/hikers-book/src/app/modules/auth/signin/signin.component.spec.ts
@@ -1,7 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MockComponent } from 'ng-mocks';
+import { APIAuthenticationAuthModule } from '../../../api/authentication/auth/api-authentication-auth.module';
 import { SocialComponent } from '../social/social.component';
 import { SignInComponent } from './signin.component';
 
@@ -11,7 +17,18 @@ describe('SignInComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, MatCardModule, MockComponent(SocialComponent)],
+      imports: [
+        HttpClientTestingModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDividerModule,
+        FormsModule,
+        ReactiveFormsModule,
+        APIAuthenticationAuthModule.forRoot({}),
+        BrowserAnimationsModule,
+        MockComponent(SocialComponent)
+      ],
       declarations: [SignInComponent]
     });
     fixture = TestBed.createComponent(SignInComponent);
